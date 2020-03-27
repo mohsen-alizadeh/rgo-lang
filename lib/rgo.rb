e2d8ae89d36a6330a2cf1314abfb2200debeaaa0
Mohsen Alizadeh <mohsen@alizadeh.us>
2020-03-27 20:07:38 +0100

    init

---
+++ lib/rgo.rb
@@ -0,0 +1,6 @@
+require "rgo/version"
+
+module Rgo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rgo_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Rgo do
+  it "has a version number" do
+    expect(Rgo::VERSION).not_to be nil
+  end
+end
